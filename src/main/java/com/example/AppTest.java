a13fd412441149ef8cf88f50f995ac6d48c6c392
Muhmad Fauzi Kuhdume <muhmad.fauzi.kuhdume@ngu.no>
2024-06-26 16:30:41 +0200

    test

--- src/main/java/com/example/AppTest.java
+++ src/main/java/com/example/AppTest.java
@@ -4,8 +4,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue() {
+    public void testApp() {
         assertTrue(true);
     }
 }
